ad359959fe2a146bf1ee60d0845188b66dcc8bd4
junaidarshad1122 <jj8286014@gmail.com>
2025-03-03 11:33:01 -0800

    data

--- src/components/Clone.jsx
+++ src/components/Clone.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Clone = () => {
   return (
     <div>
+        <h1>Git Clone Commands</h1>
       first space click <hr></hr>
       and second "my git code link pest power shell "<hr></hr>
       git add . <hr></hr>
--- src/components/Pull .jsx
+++ src/components/Pull .jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Pull  = () => {
   return (
     <div>
+        <h1>Git Pull Commands</h1>
       git pull origin master <hr></hr>
       code . <hr></hr>
     </div>
