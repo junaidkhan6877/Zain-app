cd68845035c63b2bfb52839d04bf235f8efc3f95
mzain030 <113288311+mzain030@users.noreply.github.com>
2025-03-03 11:22:02 +0500

    div and name add

--- src/components/Zain.jsx
+++ src/components/Zain.jsx
@@ -4,8 +4,10 @@ const Zain = () => {
     const [ben,setBen]=useState('');
   return (
     <div>
+      <div style={{backgroundColor:"green"}}>One</div>
       <input type="text"  placeholder='Only search' onChange={(e)=>setBen(e.target.value)} value={ben}/>
       <h1>{ben}</h1>
+      <p>Muhammd zain naseer</p>
     </div>
   )
 }
