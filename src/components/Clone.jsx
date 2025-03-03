fa58b416fbc92c495a8cc1fc938b5a172c7022aa
junaidarshad1122 <jj8286014@gmail.com>
2025-03-03 11:53:24 -0800

    data

--- src/components/Clone.jsx
+++ src/components/Clone.jsx
@@ -6,10 +6,7 @@ const Clone = () => {
         <h1>Git Clone Commands</h1>
       first space click <hr></hr>
       and second "my git code link pest power shell "<hr></hr>
-      git add . <hr></hr>
-      git push origin master <hr></hr>
-      git commit -m "branch"<hr></hr>
-      git status <hr></hr>
+     
     </div>
   )
 }
