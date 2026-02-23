4b112c1d60e5b824e9269db8e4e33c68e11d66ca
ha2548493-glitch <ha2548493@gmail.com>
2026-02-23 10:52:31 +0500

    adding

--- src/App.js
+++ src/App.js
@@ -4,28 +4,17 @@ import Zain from './components/Zain';
 import CreateNewRepo from './components/CreateNewRepo';
 import Clone from './components/Clone';
 import Pull from './components/Pull ';
-// <<<<<<< HEAD
-// import ChangingCode from './components/ChangingCode';
-// =======
-// import Maqbool from './components/Maqbool';
-// >>>>>>> a9020d22469aff967d9bd7a92cc512ac4ccc63b5
+import ChangingCode from './components/ChangingCode';
 
 function App() {
   return (
     <div className="App">
      <Zain/>
-     {/* <CreateNewRepo/>
+     <CreateNewRepo/>
      <Clone/>
-<<<<<<< HEAD
      <Pull/>
-<<<<<<< HEAD
      <ChangingCode/>
-=======
-     <Maqbool/>
->>>>>>> a9020d22469aff967d9bd7a92cc512ac4ccc63b5
-=======
-     <Pull/> */}
-{/* >>>>>>> fb5d91e089056d5f5134e75163cafc5c4dbb8454 */}
+     <Pull/>
     </div>
   );
 }
